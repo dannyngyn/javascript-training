b4ad8970a8302dbf5b8ba22601322025f41e76c3
Danny Nguyen <dnguyen5@academic.rrc.ca>
2024-08-29 13:11:08 -0500

    chapter-2
    reviewed let, var and const

---
+++ chapter_2/sandbox.js
@@ -0,0 +1,15 @@
+let age = 25;
+let year = 2019;
+
+// log age and year to console
+console.log(age, year);
+
+/* age = 30;
+console.log(age);
+
+const points = 100;
+console.log(points)
+*/
+
+var score = 75;
+console.log(score);
