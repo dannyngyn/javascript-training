eb85a27f3f2b0fecfcae6b92e2b19c0aa5988927
Danny Nguyen <dnguyen5@academic.rrc.ca>
2024-08-29 13:17:35 -0500

    chapter-2
    explored string methods

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -1,15 +1,53 @@
-let age = 25;
-let year = 2019;
+// let age = 25;
+// let year = 2019;
 
 // log age and year to console
-console.log(age, year);
+// console.log(age, year);
 
 /* age = 30;
-console.log(age);
+// console.log(age);
 
-const points = 100;
-console.log(points)
+// const points = 100;
+// console.log(points)
 */
 
-var score = 75;
-console.log(score);
+// var score = 75;
+// console.log(score);
+
+// strings
+console.log('hello, world');
+let email = 'dnguyen5@rrc.ca'
+console.log(email)
+
+// string concatenation
+let firstName = 'Danny';
+let lastName = 'Nguyen';
+
+let fullName = firstName + ' ' + lastName;
+console.log(fullName);
+
+// getting characters
+console.log(fullName[0]);
+
+// string length
+console.log(fullName.length);
+
+// string methods 
+console.log(fullName.toUpperCase());
+let result = fullName.toLowerCase();
+console.log(result, fullName);
+
+let index = email.indexOf('@');
+console.log(index);
+
+// common string methods
+//let emailResult = email.lastIndexOf('n');
+//let emailResult = email.slice(0,5);
+//let emailResult = email.substr(4,10);
+let emailResult = email.replace('d', 'w');
+
+console.log(emailResult);
+
+
+
+
