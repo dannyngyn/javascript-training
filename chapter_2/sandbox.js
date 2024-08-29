b999d2e0061063bc1988edfa038a5deae1832023
Danny Nguyen <dnguyen5@academic.rrc.ca>
2024-08-29 13:23:55 -0500

    chapter-2
    some string interpolation and methods

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -15,38 +15,98 @@
 // console.log(score);
 
 // strings
-console.log('hello, world');
-let email = 'dnguyen5@rrc.ca'
-console.log(email)
+// console.log('hello, world');
+// let email = 'dnguyen5@rrc.ca'
+// console.log(email)
 
 // string concatenation
-let firstName = 'Danny';
-let lastName = 'Nguyen';
+// let firstName = 'Danny';
+// let lastName = 'Nguyen';
 
-let fullName = firstName + ' ' + lastName;
-console.log(fullName);
+// let fullName = firstName + ' ' + lastName;
+// console.log(fullName);
 
 // getting characters
-console.log(fullName[0]);
+// console.log(fullName[0]);
 
 // string length
-console.log(fullName.length);
+// console.log(fullName.length);
 
 // string methods 
-console.log(fullName.toUpperCase());
-let result = fullName.toLowerCase();
-console.log(result, fullName);
+// console.log(fullName.toUpperCase());
+// let result = fullName.toLowerCase();
+// console.log(result, fullName);
 
-let index = email.indexOf('@');
-console.log(index);
+// let index = email.indexOf('@');
+// console.log(index);
 
 // common string methods
-//let emailResult = email.lastIndexOf('n');
-//let emailResult = email.slice(0,5);
-//let emailResult = email.substr(4,10);
-let emailResult = email.replace('d', 'w');
+// let emailResult = email.lastIndexOf('n');
+// let emailResult = email.slice(0,5);
+// let emailResult = email.substr(4,10);
+// let emailResult = email.replace('d', 'w');
 
-console.log(emailResult);
+// console.log(emailResult);
+
+let radius = 10;
+const pi = 3.14;
+
+console.log(radius, pi);
+// let radiusResult = radius % 3;
+// let radiusResult = pi * radius**2;
+let radiusResult = 5 * (10-3)**2;
+
+console.log(radiusResult);
+
+// let likes = 10;
+// likes++;
+// likes--;
+// likes+=10
+// likes-=5;
+// likes *=2;
+// likes/=2;
+
+// console.log(likes);
+
+// console.log(5 / 'hello');
+// console.log(5 * hello);
+
+// let likeResult = 'the blog has ' + likes +' likes';
+// console.log(likeResult);
+
+// template strings
+const title = 'Best reads of 2019';
+const author = 'Mario';
+const likes = 30;
+
+// concatenation way
+// let blogResult = 'The blog called ' + title + ' by' + author + ' has' + likes + ' likes';
+
+// template string way
+// let blogResult = `the blog called ${title} by ${author} by ${likes} likes`;
+// console.log(blogResult);
+
+// creating html templates
+let html = `
+ <h2>$(title)</h2>
+ <p>$(author)</p>
+ <span>This blog has ${likes}</span>
+ `;
+
+ console.log(html);
+
+ let ninjas = ['shaun', 'ryu' , 'chun-li'];
+ ninjas[1] = 'ken';
+ console.log(ninjas[1])
+
+let ages = [20,25,30.35];
+console.log(ages[2]);
+
+let random = ['shaun', 'crystal', 30,20];
+console.log(random);
+
+console.log(ninjas.length)
+
 
 
 
