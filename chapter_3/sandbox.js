cf35b9fddd98ac7b9174851197ef9486b692b382
Danny Nguyen <dnguyen5@academic.rrc.ca>
2024-08-29 15:20:31 -0500

    chapter-3
    break and continue

--- chapter_3/sandbox.js
+++ chapter_3/sandbox.js
@@ -60,4 +60,19 @@ else{
     console.log('password is not long enough')
 }
 
+const scores = [50,25,0,30,100,20,10];
+
+for(let i = 0; i < scores.length; i++){
+
+    if(scores[i] === 0){
+        continue;
+    }
+
+    console.log('your score: ', scores[i]);
+
+    if(scores[i] === 100){
+        console.log('congrats, you go the top score');
+        break;
+    }
+}
 
