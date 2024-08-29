576adec5beb5889b7232e973a2450b503d6a7fde
Danny Nguyen <dnguyen5@academic.rrc.ca>
2024-08-29 14:43:53 -0500

    chapter-2
    finished notes

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -162,17 +162,32 @@
 // true capitals are greater 
 // console.log(name > 'Shaun')
 
-let age = 25;
+// let age = 25;
 
 // loose comparison
 
-console.log(age == 25)
-console.log(age == '25')
+// console.log(age == 25)
+// console.log(age == '25')
 
 // strict comparison
 
-console.log(age === 25);
-console.log(age === '25');
+// console.log(age === 25);
+// console.log(age === '25');
 
-console.log(age !== 25);
-console.log(age !== '25');
+// console.log(age !== 25);
+// console.log(age !== '25');
+
+//type conversion
+// let score = '100';
+
+// score = Number(score)
+// console.log(score + 1);
+// console.log(typeof score);
+
+// let result = String(50);
+// console.log(result, typeof result);
+
+// let result = Boolean(100);
+
+// let result = Boolean(0);
+// console.log(result);
