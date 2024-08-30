2482a40a7c6a2190f44a5f1da6589a7aa0c9f2ac
Danny Nguyen <dnguyen5@academic.rrc.ca>
2024-08-30 08:27:47 -0500

    chapter-3
    another arrow function

--- chapter_4/sandbox.js
+++ chapter_4/sandbox.js
@@ -1,15 +1,20 @@
-// regular function
-// const calcArea = function(radius){
-//     return 3.14 * radius**2;
-// };
-
 // arrow function
-// const calcArea = (radius) => {
-//     return 3.14 * radius**2;
+
+// const bill = function(products, tax){
+//     let total = 0;
+//     for(let i = 0; i < products.length; i++){
+//         total += products[i] + products[i] * tax
+//     }
+//     return total;
 // };
 
-// if one paramater values
-const calcArea = radius => 3.14 * radius**2;
+const bill = (products,tax) => {
+    let total = 0;
+    for(let i = 0; i < products.length; i++){
+        total += products[i] + products[i] * tax
+    }
+    return total;
+};
 
-const area = calcArea(5);
-console.log('area is: ', area)
+console.log(bill[10,15,30],0.2);
+ 
